07f15a7d90d8ab17afeb52a1c7c69f31ab8f9333
defiscout <farmerlesheng@gmail.com>
2024-01-08 18:46:47 +0800

    add blockchain

---
+++ blockchain.py
@@ -0,0 +1,124 @@
+import hashlib
+import json
+from time import time
+from typing import Any, Dict, List, Optional
+
+
+class Blockchain:
+    def __init__(self):
+        self.current_transactions = []
+        self.chain = []
+
+        # 创建创世块
+        self.new_block(previous_hash='1', proof=100)
+
+
+    def new_block(self, proof: int, previous_hash: Optional[str]) -> Dict[str, Any]:
+        """
+        生成新块
+
+        :param proof: The proof given by the Proof of Work algorithm
+        :param previous_hash: Hash of previous Block
+        :return: New Block
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # Reset the current list of transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender: str, recipient: str, amount: int) -> int:
+        """
+        生成新交易信息，信息将加入到下一个待挖的区块中
+
+        :param sender: Address of the Sender
+        :param recipient: Address of the Recipient
+        :param amount: Amount
+        :return: The index of the Block that will hold this transaction
+        """
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
+
+    @property
+    def last_block(self) -> Dict[str, Any]:
+        return self.chain[-1]
+
+    @staticmethod
+    def hash(block: Dict[str, Any]) -> str:
+        """
+        生成块的 SHA-256 hash值
+
+        :param block: Block
+        """
+
+        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    def proof_of_work(self, last_proof: int) -> int:
+        """
+        简单的工作量证明:
+         - 查找一个 p' 使得 hash(pp') 以4个0开头
+         - p 是上一个块的证明,  p' 是当前的证明
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof: int, proof: int) -> bool:
+        """
+        验证证明: 是否hash(last_proof, proof)以4个0开头
+
+        :param last_proof: Previous Proof
+        :param proof: Current Proof
+        :return: True if correct, False if not.
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+
+# transaction1
+blockchain.new_transaction(
+        sender="0",
+        recipient="abc-efg-xyz",
+        amount=1,
+    )
+
+# transaction2
+blockchain.new_transaction(
+        sender="xxxxxxx",
+        recipient="abc-efg-xyz",
+        amount=100,
+    )
+
+last_proof = blockchain.last_block['proof']
+proof = blockchain.proof_of_work(last_proof)
+
+blockchain.new_block(proof, None)
+
+print(json.dumps(blockchain.chain, sort_keys=True))
+
+
